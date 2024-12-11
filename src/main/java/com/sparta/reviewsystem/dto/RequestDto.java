8f84bda5e40d53462591274d6dbc976328b5011c
aeongiii <ljhkys6874@naver.com>
2024-12-11 17:29:30 +0900

    RequestDto, ResponseDto 생성

---
+++ src/main/java/com/sparta/reviewsystem/dto/RequestDto.java
@@ -0,0 +1,11 @@
+package com.sparta.reviewsystem.dto;
+
+public class RequestDto {
+
+    // 요청부에는 userId, score, content가 있다.
+    private Long userId;
+
+    private Float score;
+
+    private String content;
+}
---
+++ src/main/java/com/sparta/reviewsystem/dto/ResponseDto.java
@@ -0,0 +1,19 @@
+package com.sparta.reviewsystem.dto;
+
+import java.time.LocalDateTime;
+
+public class ResponseDto {
+
+    private Long id;
+
+    private Long userId;
+
+    private Float score;
+
+    private String content;
+
+    private String imageUrl;
+
+    private LocalDateTime createdAt;
+
+}
