e309ad15d6a0abeabe249e797d6824783aada1e6
aeongiii <ljhkys6874@naver.com>
2024-12-13 01:38:35 +0900

    [refactor] 서비스 코드 리팩토링
    - 핵심 메서드인 createReview, getReview에서 가독성을 저하시키는 로직 캡슐화
    - 불필요한 로직 간소화

--- src/main/java/com/sparta/reviewsystem/service/ReviewService.java
+++ src/main/java/com/sparta/reviewsystem/service/ReviewService.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -31,47 +32,23 @@ public class ReviewService {
     @Transactional
     public void createReview(Long productId, RequestDto requestDto, String imageUrl) {
 
-        // 1. 상품이 존재하는지 먼저 확인
-        Product product = productRepository.findById(productId)
-                .orElseThrow(() -> new IllegalArgumentException("상품이 존재하지 않습니다. productId : " + productId));
+        // 상품이 존재하는지 확인
+        Product product = findProductById(productId);
 
-        // 2. 중복체크
+        // 리뷰 중복체크
         if (reviewRepository.existsByProductIdAndUserId(productId, requestDto.getUserId())) {
             throw new IllegalArgumentException("이미 리뷰를 작성했습니다.");
         }
 
-
-        // 3. 리뷰 저장하기
-        Review review = new Review();
-        review.setProduct(product);
-        review.setUserId(requestDto.getUserId());
-        review.setContent(requestDto.getContent());
-        review.setUserScore(requestDto.getScore());
-        review.setImageUrl(imageUrl); // 입력받지 않았다면 null이 그대로 들어간다.
-
-        // 작성시간 -> 알아서 찍혀 들어간다.
-//        review.setCreateAt(requestDto.getCreateAt());
-
+        // 리뷰 저장
+        Review review = buildReview(product, requestDto, imageUrl);
         reviewRepository.save(review);
 
-        // 4. Product에 있는 내용도 수정한다.
         // 총 리뷰개수 +1
         product.setReviewCount(product.getReviewCount() + 1);
 
-        // 총 평점 변경 : 해당 제품에 대한 리뷰 다 뽑아와서, 별점 새로 계산한다.
-        List<Review> reviewList = reviewRepository.findByProductId(productId);
-        float averageScore = (float) reviewList.stream()
-                .mapToDouble(Review::getUserScore) // 각 객체에서 userScore 추출, .average() 사용하기 위해 double로 변환
-                .average()
-                .orElse(0.0);
-        product.setScore(averageScore);
-
-        // Product 저장(수정)
-        productRepository.save(product);
-        
-
-
-
+        // 평균 점수 계산 및 저장
+        calculateAndSaveAverageScore(productId, product);
 
     }
 
@@ -79,54 +56,85 @@ public class ReviewService {
     @Transactional
     public ReviewDto getReview(Long productId, Long cursor, int size) {
 
-        // 1. 상품이 존재하는지 먼저 확인
-        productRepository.findById(productId)
-                .orElseThrow(() -> new IllegalArgumentException("상품이 존재하지 않습니다. productId : " + productId));
+        // 상품이 존재하는지 확인
+        Product product = findProductById(productId);
 
-        // 총 리뷰 평균 계산
-        List<Review> allReview = reviewRepository.findByProductId(productId);
-        float averageScore = (float) allReview.stream()
+        // 평균 가져오기 & 리뷰 총 개수 계산
+        float averageScore = product.getScore();
+        Long totalReviewCount = reviewRepository.countByProductId(productId);
+
+        // cursor값 기준으로 페이징해서 리스트로 가져오기
+        List<ReviewResponseDto> reviewResponses = fetchPagedReviews(productId, cursor, size);
+
+        // 다음에 반환할 리뷰 남았다면 nextCursor 반환. 리뷰 없다면 0 반환
+        Long nextCursor = getNextCursor(reviewResponses);
+
+        // 리턴
+        return new ReviewDto (totalReviewCount, averageScore, nextCursor, reviewResponses);
+    }
+
+
+    // ===========
+
+
+    // 리뷰 등록 전에 해당 상품이 있는지 확인
+    private Product findProductById(Long productId) {
+        return productRepository.findById(productId)
+                .orElseThrow(() -> new IllegalArgumentException("상품이 존재하지 않습니다. productId : " + productId));
+    }
+
+    // 리뷰 저장
+    private Review buildReview(Product product, RequestDto requestDto, String imageUrl) {
+        Review review = new Review();
+        review.setProduct(product);
+        review.setUserId(requestDto.getUserId());
+        review.setContent(requestDto.getContent());
+        review.setUserScore(requestDto.getScore());
+        review.setImageUrl(imageUrl);
+        return review;
+    }
+
+    // 평균값 계산하고 저장
+    private void calculateAndSaveAverageScore(Long productId, Product product) {
+        List<Review> allReviews = reviewRepository.findByProductId(productId);
+        float averageScore = (float) allReviews.stream()
                 .mapToDouble(Review::getUserScore)
                 .average()
                 .orElse(0.0);
-//        System.out.println("averageScore = " + averageScore);
 
-        // 전체 리뷰 수 계산
-        Long totalReviewCount = reviewRepository.countByProductId(productId);
-//        System.out.println("totalReviewCount = " + totalReviewCount);
+        product.setScore(averageScore);
+        productRepository.save(product);
 
-        // 2. 페이징 설정
-        Pageable pageable = PageRequest.of(0, size); // 0번부터 size까지의 페이지 정보 반환
+    }
+
+    // cursor값 기준으로 페이징해서 리스트로 가져오기
+    private List<ReviewResponseDto> fetchPagedReviews(Long productId, Long cursor, int size) {
+        Pageable pageable = PageRequest.of(0, size);
         Slice<Review> reviewSlice;
 
-        if (cursor == 0) { // 첫페이지 불러올 경우 최신순 정렬한 뒤 첫페이지 가져오기
-            reviewSlice = reviewRepository.findByProduct_IdOrderByCreatedAtDesc(
-                    productId, pageable);
-        } else { // 첫페이지 아닌 경우 cursor 값 기준으로 다음 페이지 가져오기
-            reviewSlice = reviewRepository.findByProduct_IdAndIdLessThanOrderByCreatedAtDesc(
-                    productId, cursor, pageable);
+        if (cursor == 0) {
+            reviewSlice = reviewRepository.findByProduct_IdOrderByCreatedAtDesc(productId, pageable);
+        } else {
+            reviewSlice = reviewRepository.findByProduct_IdAndIdLessThanOrderByCreatedAtDesc(productId, cursor, pageable);
         }
 
-        // 반환된 reviewSlice를 ReviewResponseDto 리스트로 바꾸기
-        // getContent() : 실제 데이터 리스트 반환
-        List<ReviewResponseDto> reviewResponses = reviewSlice.getContent().stream()
-                .map(review -> new ReviewResponseDto (
+        return reviewSlice.getContent().stream()
+                .map(review -> new ReviewResponseDto(
                         review.getId(),
                         review.getUserId(),
                         review.getUserScore(),
                         review.getContent(),
                         review.getImageUrl(),
                         review.getCreatedAt()
-                )).toList();
-//        System.out.println("reviewResponses.size() = " + reviewResponses.size());
-
-        // 다음 값 있으면 nextCursor 값 반환
-        Long nextCursor = reviewSlice.hasNext() ? reviewResponses.get(reviewResponses.size() -1).getId() : null;
-
-        // 반환할 값 만들기
-        ReviewDto reviewDto = new ReviewDto (totalReviewCount, averageScore, nextCursor, reviewResponses);
-
-
-        return reviewDto;
+                ))
+                .collect(Collectors.toList());
     }
+
+    // 다음에 반환할 리뷰 남았다면 nextCursor 반환. 리뷰 없다면 0 반환
+    private Long getNextCursor(List<ReviewResponseDto> reviews) {
+        return reviews.isEmpty() ? 0 : reviews.get(reviews.size() - 1).getId();
+    }
+
+
+
 }
